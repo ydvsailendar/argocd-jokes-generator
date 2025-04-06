d646e3159e4987ecec02a1566ae4d141f2950aa5
ydvsailendar <ydvsailendar@gmail.com>
2025-04-06 10:37:48 +0100

    set ingress to access app and argocd/dashboard

--- cp.tf
+++ cp.tf
@@ -24,6 +24,24 @@ resource "kubernetes_service_account" "k8s" {
   depends_on = [kubernetes_namespace.jokes]
 }
 
+resource "google_project_iam_member" "k8s_lb" {
+  project = data.google_client_config.current.project
+  role    = "roles/compute.loadBalancerAdmin"
+  member  = "serviceAccount:${google_service_account.sa.email}"
+}
+
+resource "google_project_iam_member" "k8s_cert" {
+  project = data.google_client_config.current.project
+  role    = "roles/compute.securityAdmin"
+  member  = "serviceAccount:${google_service_account.sa.email}"
+}
+
+resource "google_project_iam_member" "k8s_dns" {
+  project = data.google_client_config.current.project
+  role    = "roles/dns.admin"
+  member  = "serviceAccount:${google_service_account.sa.email}"
+}
+
 resource "google_service_account_iam_binding" "k8s" {
   service_account_id = google_service_account.sa.id
   role               = "roles/iam.workloadIdentityUser"
